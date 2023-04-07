8f549632d1d9abde15610ab8767cf9fc57aa5f81
briankim <brianhkim13@gmail.com>
2023-04-06 20:37:51 -0500

    added input and onChange event listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,44 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 const cohort = "2301-ftb-et-web-am"
 
 function App() {
-  useEffect(()=>{
-    fetchPlayers()
-  },[])
-  const [players, setPlayers] = useState([])
+  const [PuppyData, setPuppyData] = useState([]);
+  const [puppyId, setPuppyId] = useState(1);
+  const [error, setError] = useState(null);
 
-  async function fetchPlayers(){
+  useEffect(()=>{
+  async function FetchPlayers(){
     try{
       const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/${cohort}/players`)
       // const result = await response.json();
       const { data } = await response.json() //data: {players: []}
-      const { players } = data; //[]
-      await setPlayers(players)
+      const { players } =await data; 
+      const PuppyData = {players}//[]
+      await setPuppyData(PuppyData)
+      console.log(PuppyData)
     } catch (err){
-      console.error(err)
+      setError(error.message);
     }
   }
-console.log(players)
+  FetchPlayers();
+  },[puppyId]);
+
+console.log(PuppyData)
 
   
   return (
     <div className="App">
-      <div>Hello World</div>
+      <div>Players</div>
+      <div>
+        <input type="text" value={puppyId} onChange={event => setPuppyId(event.target.value)}/>
+        <p>PuppyId: {puppyId}</p>
+        {PuppyData && <p>{PuppyData.name}</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {/* {players.map((dog)=>{
+        return <FetchPlayers key={dog.id}
+      })} */}
     </div>
   )
 }
