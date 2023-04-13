8494c0077eda9707a269a322f30995b0d1beb378
briankim <brianhkim13@gmail.com>
2023-04-12 21:13:04 -0500

    Onclick single Player but form disappered

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import AllPlayers from "./components/AllPlayers";
 import NewDogPlayer from "./components/NewDogPlayer";
+import SingleDog from "./components/SinglePlayer";
 function App() {
   return (
     <div className="App">
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<AllPlayers />} />
           <Route path="/form" element={<NewDogPlayer />} />
-          <Route path="/:dogId" element={<singlePlayer />} />
+          <Route path="/:dogId" element={<SingleDog />} />
         </Routes>
       </div>
     </div>
--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -1,33 +1,30 @@
-import React, {useEffect, useState} from "react";
-import {fetchSingleDog}
-
-function SinglePlayer() {
-  return <h2>SIngle Player</h2>;
-}
+import React, { useEffect, useState } from "react";
+import { fetchSingleDog } from "../api/index";
+import { useParams } from "react-router-dom";
 
 export default function SingleDog() {
-    const { dogId } = useParams();
-  
-    const [dog, setDog] = useState(null);
-  
-    useEffect(() => {
-      async function getDog() {
-        const dogFromApi = await fetchSingleDog(dogId);
-        setDog(dogFromApi);
-      }
-      getDog();
-    }, []);
-  
-    console.log("Dog: ", dog);
-  
-    return (
-      <div>
-        {dog && (
-          <ul>
-            <li>Name: {dog.name}</li>
-            <li>Age: {dog.age}</li>
-          </ul>
-        )}
-      </div>
-    );
-  }
+  const { dogId } = useParams();
+
+  const [dog, setDog] = useState(null);
+
+  useEffect(() => {
+    async function getDog() {
+      const dogFromApi = await fetchSingleDog(dogId);
+      setDog(dogFromApi);
+    }
+    getDog();
+  }, []);
+
+  console.log("Dog: ", dog);
+
+  return (
+    <div>
+      {dog && (
+        <ul>
+          <li>Name: {dog.name}</li>
+          <li>Age: {dog.age}</li>
+        </ul>
+      )}
+    </div>
+  );
+}
